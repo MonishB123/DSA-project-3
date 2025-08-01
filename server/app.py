0f6bd074b063937658e22b0dd85c25ff1f1728f9
MonishB123 <monishbeegamudre@gmail.com>
2025-08-01 01:08:09 -0400

    updated flask api for getting day states, also updated js to update table with data

--- server/app.py
+++ server/app.py
@@ -11,12 +11,31 @@ def getVariants():
     data = request.get_json()
     country = data["country"]
     
-    #base data (testing API)
+    #example data (testing API)
     countryVariants = {
-        "United States" : ["B12", "Novid", "Idk", "OtherVariant"],
+        "United States" : ["B12", "Novid"],
         "France" : ["Norovirus", "C19"]
     }
     data = {"variants" : countryVariants[country]}
+    return jsonify(data)
+
+#Given a country name and variant, return a dict with dates and the number of infections on that day
+@app.route('/get-day-stats', methods=["POST"])
+def getDayStats():
+    data = request.get_json()
+    country = data["country"]
+    variant = data["variant"]
+
+    #example data (testing API)
+    dailyData = {
+        "United States" : {"B12" : {"10-12-21" : 2,
+                                    "10-13-21" : 3,
+                                    "10-14-21" : 10,}},
+        "France" : {"Norovirus" : {"10-10-21" : 10,
+                                    "10-11-21" : 2,
+                                    "10-12-21" : 10,}},                
+    }
+    data = {"info" : dailyData[country][variant]}
     return jsonify(data)
 
 
--- web/script.js
+++ web/script.js
@@ -1,8 +1,11 @@
-const countrySearchButton = document.getElementById("search-button");
 
+var currCountry;
+
+const countrySearchButton = document.getElementById("search-button");
 countrySearchButton.addEventListener('keyup', (event)=>{
     if(event.key == 'Enter'){
         var button = document.getElementById("country-value");
+        currCountry = button.value;
         const data = {
             country : button.value
         };
@@ -19,7 +22,11 @@ countrySearchButton.addEventListener('keyup', (event)=>{
         })
         .then(variantData =>{
             var variantSelect = document.getElementById("variants");
+
+            const variantElem = document.createElement("option");
             variantSelect.innerHTML = "";
+            variantElem.textContent = "";
+            variantSelect.append(variantElem);
             for(const variant of variantData['variants']){
                 const variantElem = document.createElement("option");
                 variantElem.textContent = variant;
@@ -30,3 +37,37 @@ countrySearchButton.addEventListener('keyup', (event)=>{
 
     }
 });
+
+const variantSelector = document.getElementById("variants");
+variantSelector.addEventListener('change', function(){
+    const currVariant = this.value;
+    data = {country : currCountry, variant : currVariant};
+    fetch("http://127.0.0.1:5000/get-day-stats", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .then(data =>{
+            dateData = data['info']
+            var dailyTable = document.getElementById("daily-table")
+            for(const date of Object.keys(dateData)){
+                const row = document.createElement("tr");
+
+                const dateElem = document.createElement("td");
+                dateElem.textContent = date;
+                const infectionNum = document.createElement("td");
+                infectionNum.textContent = dateData[date];
+                row.append(dateElem);
+                row.append(infectionNum)
+
+                dailyTable.append(row);
+            }
+        });
+});
+
